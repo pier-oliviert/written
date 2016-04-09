c25a2028b0a2a1a079d15b5e698dc5dd3727cdcc
pothibo <pothibo@gmail.com>
2016-04-09 12:00:08 -0400

    Remove Ruby parsers

--- lib/written.rb
+++ lib/written.rb
@@ -1,12 +1,4 @@
 module Written
-  autoload :Document, 'written/document'
-  autoload :Version,  'written/version'
-
-  def self.parse(str)
-    document = Document.new(str)
-    document.parse!
-    document
-  end
 end
 
 if defined?(Rails)
