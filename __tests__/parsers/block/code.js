07f5ce687213704aa60b8bbd414459fc90f78a66
Pier-Olivier Thibault <pothibo@gmail.com>
2016-05-24 06:41:28 -0400

    Initial block test. Might indicate that the APIs for the blocks aren't that great still

--- __tests__/parsers/block/code.js
+++ __tests__/parsers/block/code.js
@@ -2,7 +2,7 @@ let struct = undefined;
 
 window.Written = {
   Parsers: {
-    Inline: {},
+    Block: {},
     register: function(s) {
       struct = s
     }
@@ -14,28 +14,45 @@ require('../../../lib/written/app/assets/javascripts/written/parsers/block/code'
 
 describe('code', () => {
   it('detects a match', () => {
-    let match = struct.rule.exec('*test*')
+    let match = struct.rule.exec('~~~')
     expect(match).not.toBeNull()
   })
 
-  it('ignores text that do not match', () => {
-    let match = struct.rule.exec('test*')
-    expect(match).toBeNull()
+  it('can add lines until it finds the closing tag', () => {
+    let match = struct.rule.exec('~~~')
+    let parser = new struct.parser(match)
+
+    expect(match).not.toBeNull()
+    expect(parser.accepts('let var = true')).toBeTruthy()
+    parser.append('let var = true')
+
+    expect(parser.accepts('~~~')).toBeTruthy()
+    parser.append('~~~')
+
+    expect(parser.opened).toBeFalsy()
   })
 
   it('returns the content for the editor', () => {
-    struct.rule.lastIndex = 0
-    let match = struct.rule.exec('*test*')
-    expect(match).toBeTruthy()
+    let match = struct.rule.exec('~~~')
+    let html = Written.toHTML('<pre><code>~~~\nlet var = true\n~~~</code></pre>')
+
     let parser = new struct.parser(match)
-    expect(parser.toEditor().outerHTML).toBe(Written.toHTML('<strong>*test*</strong>').outerHTML)
+    parser.append('let var = true')
+    parser.append('~~~')
+
+    expect(parser.opened).toBeFalsy()
+    expect(parser.toEditor().outerHTML).toBe(html.outerHTML)
   })
 
   it('HTML content should not include the markdown syntax', () => {
-    struct.rule.lastIndex = 0
-    let match = struct.rule.exec('*test*')
-    expect(match).toBeTruthy()
+    let match = struct.rule.exec('~~~')
+    let html = Written.toHTML('<pre><code>\nlet var = true\n</code></pre>')
+
     let parser = new struct.parser(match)
-    expect(parser.toHTML().outerHTML).toBe(Written.toHTML('<strong>test</strong>').outerHTML)
+    parser.append('let var = true')
+    parser.append('~~~')
+
+    expect(parser.opened).toBeFalsy()
+    expect(parser.toHTML().outerHTML).toBe(html.outerHTML)
   })
 })
