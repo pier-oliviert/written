e597cf618f35113236a146aa7a53d03d46b8eec2
pothibo <pothibo@gmail.com>
2016-05-26 17:30:59 -0400

    Initial attempt at making this fun to read :)

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+document.removeEventListener('DOMContentLoaded', Prism.highlightAll)
+
+Written.Parsers.get('pre').highlightWith(function(element) {
+  Prism.highlightElement(element, false)
+})
+
+Written.Parsers.get('code').highlightWith(function(element) {
+  Prism.highlightElement(element, false)
+})
+
 var el = document.querySelector('main')
 
 editor = new Written(el)
