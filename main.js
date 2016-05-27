59589a5374fe52be44e32474ac289fdda29f1996
pothibo <pothibo@gmail.com>
2016-05-27 08:20:31 -0400

    Allow to toggle between rendered version and editor

--- main.js
+++ main.js
@@ -8,7 +8,41 @@ Written.Parsers.get('code').highlightWith(function(element) {
   Prism.highlightElement(element, false)
 })
 
-var el = document.querySelector('main')
+var main = document.querySelector('main')
+var Editor = document.querySelector('#Editor')
+var Rendered = document.querySelector('#Rendered')
+var toggles = Array.prototype.slice.call(document.querySelectorAll('nav > a'))
+var written = new Written(Editor)
 
-editor = new Written(el)
-editor.initialize()
+written.initialize()
+
+toggles[0].addEventListener('click', function(e) {
+  if (Editor.parentElement == null) {
+    Rendered.remove()
+    main.appendChild(Editor)
+    delete e.target.parentElement.querySelector('[data-selected]').dataset.selected
+    e.target.dataset.selected = true
+  }
+})
+
+toggles[1].addEventListener('click', function(e) {
+  if (Rendered.parentElement == null) {
+    Editor.remove()
+    main.appendChild(Rendered)
+    delete e.target.parentElement.querySelector('[data-selected]').dataset.selected
+    e.target.dataset.selected = true
+  }
+})
+
+Editor.addEventListener('written:changed', function() {
+  Rendered.innerHTML = written.history.current.toHTMLString()
+  Array.prototype.slice.call(Rendered.querySelectorAll('code')).forEach(function(code) {
+    Prism.highlightElement(code, true)
+  })
+})
+
+Rendered.innerHTML = written.history.current.toHTMLString()
+Array.prototype.slice.call(Rendered.querySelectorAll('code')).forEach(function(code) {
+    Prism.highlightElement(code, true)
+  })
+Rendered.remove()
