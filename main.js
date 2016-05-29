226efe8b0071ccda7f31770567b0591cdea92e7e
pothibo <pothibo@gmail.com>
2016-05-29 12:55:51 -0400

    Rework the way the doc is layed out

--- main.js
+++ main.js
@@ -1,48 +1,69 @@
 document.removeEventListener('DOMContentLoaded', Prism.highlightAll)
 
-parsers = new Written.Parsers()
-parsers.get('pre').highlightWith(function(element) {
-  Prism.highlightElement(element, false)
-})
+Array.prototype.slice.call(document.querySelectorAll('main > section')).forEach( function(section) {
+  var editor = section.querySelector('.editor')
+  var rendered = section.querySelector('.rendered')
+  var toggles = Array.prototype.slice.call(section.querySelectorAll('nav > a'))
+  var blocks, inlines
 
-parsers.get('code').highlightWith(function(element) {
-  Prism.highlightElement(element, false)
-})
-
-var main = document.querySelector('main')
-var Editor = document.querySelector('#Editor')
-var Rendered = document.querySelector('#Rendered')
-var toggles = Array.prototype.slice.call(document.querySelectorAll('nav > a'))
-var written = new Written(Editor, {parsers: parsers})
-
-toggles[0].addEventListener('click', function(e) {
-  if (Editor.parentElement == null) {
-    Rendered.remove()
-    main.appendChild(Editor)
-    delete e.target.parentElement.querySelector('[data-selected]').dataset.selected
-    e.target.dataset.selected = true
+  if (editor.dataset.blocks) {
+    blocks = Written.Parsers.Blocks.select.apply(Written.Parsers.Blocks, editor.dataset.blocks.split(','))
   }
-})
 
-toggles[1].addEventListener('click', function(e) {
-  if (Rendered.parentElement == null) {
-    Editor.remove()
-    main.appendChild(Rendered)
-    delete e.target.parentElement.querySelector('[data-selected]').dataset.selected
-    e.target.dataset.selected = true
+  if (editor.dataset.inlines) {
+    inlines = Written.Parsers.Inlines.select.apply(Written.Parsers.Inlines, editor.dataset.inlines.split(','))
   }
-})
 
-Editor.addEventListener('written:changed', function() {
-  Rendered.innerHTML = written.history.current.toHTMLString()
-  Array.prototype.slice.call(Rendered.querySelectorAll('code')).forEach(function(code) {
-    Prism.highlightElement(code, false)
-  })
-})
+  parsers = new Written.Parsers(blocks, inlines)
 
-Rendered.innerHTML = written.history.current.toHTMLString()
-Array.prototype.slice.call(Rendered.querySelectorAll('code')).forEach(function(code) {
-    Prism.highlightElement(code, false)
+  if (parsers.get('pre')) {
+    parsers.get('pre').highlightWith(function(element) {
+      Prism.highlightElement(element, false)
+    })
+  }
+
+  if (parsers.get('code')) {
+    parsers.get('code').highlightWith(function(element) {
+      Prism.highlightElement(element, false)
+    })
+  }
+
+
+  var written = new Written(editor, {parsers: parsers})
+
+  toggles[0].addEventListener('click', function(e) {
+    if (editor.parentElement == null) {
+      section.insertBefore(editor, rendered)
+      rendered.remove()
+      delete e.target.parentElement.querySelector('[data-selected]').dataset.selected
+      e.target.dataset.selected = true
+    }
   })
 
-Editor.remove()
+  toggles[1].addEventListener('click', function(e) {
+    if (rendered.parentElement == null) {
+      section.insertBefore(rendered, editor)
+      editor.remove()
+      delete e.target.parentElement.querySelector('[data-selected]').dataset.selected
+      e.target.dataset.selected = true
+    }
+  })
+
+  editor.addEventListener('written:changed', function() {
+    rendered.innerHTML = written.history.current.toHTMLString()
+    Array.prototype.slice.call(rendered.querySelectorAll('code')).forEach(function(code) {
+      Prism.highlightElement(code, false)
+    })
+  })
+
+  rendered.innerHTML = written.history.current.toHTMLString()
+  Array.prototype.slice.call(rendered.querySelectorAll('code')).forEach(function(code) {
+      Prism.highlightElement(code, false)
+    })
+
+  editor.remove()
+});
+
+Array.prototype.slice.call(document.querySelectorAll('pre[data-highlight=true]')).forEach(function(block) {
+  Prism.highlightElement(block, false)
+})
